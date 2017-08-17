3b3d627c983496a953cdd58a8c171309213a7762
Rodrigo Dennis Perea Camargo <rdp20@kant.nmr.mgh.harvard.edu>
2017-08-17 18:40:19 -0400

    Changes made in rotrk_ROIxyz, issues with filetype checking

--- rotrk_ROIxyz.m
+++ rotrk_ROIxyz.m
@@ -21,38 +21,38 @@ end
 %If roi_input is in structure form (e.g. roi_input.id and
 %roi_input.filename)
 if isstruct(roi_input)
-    roi_filename=cell2char(roi_input.filename);
-    roi_mean_xyz.id=roi_input.id;
+    roi_xyz.filename=cell2char(roi_input.filename);
+    roi_xyz.id=roi_input.id;
 elseif iscell(roi_input)
     roi_filenamecell2char(roi_input);
-    roi_mean_xyz.id='No ID!';
+    roi_xyz.id='No ID!';
 else
-    roi_filename= roi_input;
+    roi_xyz.filename= roi_input;
     if nargin < 2
-        roi_mean_xyz.id='No ID, since roi_input.filename was not input (no in struct form)!';
+        roi_xyz.id='No ID, since roi_input.filename was not input (no in struct form)!';
     else
-        roi_mean_xyz.id=file_name;
+        roi_xyz.id=file_name;
         roi_xyz.id = file_name;
     end
 end
 %Is it gzip?
-[ roi_dir , roi_name , roi_ext ] = fileparts(roi_filename);
+[ roi_dir , roi_name , roi_ext ] = fileparts(roi_xyz.filename);
 if strcmp(roi_ext,'.gz')
-    system(['gunzip -f ' roi_filename]);
+    system(['gunzip -f ' roi_xyz.filename]);
     if isempty(roi_dir)
-        roi_filename = [ '.' filesep filesep roi_name ]; 
+        roi_xyz.filename = [ '.' filesep filesep roi_name ]; 
     else
-        roi_filename = [ roi_dir filesep roi_name ];
+        roi_xyz.filename = [ roi_dir filesep roi_name ];
     end
 end
 %Read the volume:
-H_vol = spm_vol(roi_filename);
+H_vol = spm_vol(roi_xyz.filename);
 mat2=H_vol.mat;
 %Read the vols:
 V_vol=spm_read_vols(H_vol);
 
 %was it gzipped?
- if strcmp(roi_ext,'.gz');  system(['gzip -f ' roi_filename ]) ; end
+ if strcmp(roi_ext,'.gz');  system(['gzip -f ' roi_xyz.filename ]) ; end
  
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
