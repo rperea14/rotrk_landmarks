c35eecbadda719aade1301c7dd960f91b9d1b312
Rodrigo Dennis Perea Camargo <rdp20@kant.nmr.mgh.harvard.edu>
2018-04-12 16:16:31 -0400

    Move files so those called by the Object are in a parent directory and other under ./dev

--- rotrk_length.m
+++ rotrk_length.m
@@ -1,21 +1,26 @@
-function [ length_val ] = rotrk_length(streamlines)
+function [ LENGHT_VAL ] = rotrk_length(streamlines)
 % function [ length_val ] = rotrk_length(streamlines)
 % Goal: Given a N x 3 coordinate for a given streamline, we calculate the
 % length of the streamline using a one-to-one point base eucledian
 % distances 
 
-if ~isnum(streamlines)
+if ~isnumeric(streamlines)
     error('Invalid streamlines aguments. Make sure you pass single streamliens')
 end
 
 
 if size(streamlines,2) == 3
-    dsiplay('Streamline class and size is ok');
+    display('Streamline class and size is ok..getting the length now');
 elseif size(streamlines,2) >3 
    warning('streamlines arguments has more than 3 colums. Assuming xyz coordiantes in the first three columsn and omitting the others')
 else
     error('Incorrect number of streamlines. You need at least 3 columns for a streamlines of size N x 3');
 end
 
+cur_len=0;
+for jj=1:size(streamlines,1)-1
+    cur_len=cur_len+pdist2(streamlines(jj,:),streamlines(jj+1,:));
+end
 
+LENGHT_VAL = cur_len;
 
